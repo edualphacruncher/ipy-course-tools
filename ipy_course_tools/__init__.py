ff131c4cd4cf7289644631fbca194edd1e70d3c8
Mate Kovacs <mate.kovacs@alphacruncher.com>
2021-11-19 14:44:04 +0100

    Updates

---
+++ ipy_course_tools/__init__.py
@@ -0,0 +1,2 @@
+__version__ = "0.1.0"  #: the working version
+__release__ = "0.1.0"  #: the release version
--- setup.py
+++ setup.py
@@ -3,31 +3,31 @@ import re
 
 from setuptools import find_packages, setup
 
-regexp = re.compile(r'.*__version__ = [\'\"](.*?)[\'\"]', re.S)
+regexp = re.compile(r".*__version__ = [\'\"](.*?)[\'\"]", re.S)
 
-base_package = 'ipy_course_tools'
+base_package = "ipy_course_tools"
 base_path = os.path.dirname(__file__)
 
-init_file = os.path.join(base_path, 'src', 'ipy_course_tools', '__init__.py')
-with open(init_file, 'r') as f:
+init_file = os.path.join(base_path, "ipy_course_tools", "__init__.py")
+with open(init_file, "r") as f:
     module_content = f.read()
 
     match = regexp.match(module_content)
     if match:
         version = match.group(1)
     else:
-        raise RuntimeError(
-            'Cannot find __version__ in {}'.format(init_file))
+        raise RuntimeError("Cannot find __version__ in {}".format(init_file))
 
-with open('README.rst', 'r') as f:
+with open("README.rst", "r") as f:
     readme = f.read()
 
-with open('CHANGELOG.rst', 'r') as f:
+with open("CHANGELOG.rst", "r") as f:
     changes = f.read()
 
+
 def parse_requirements(filename):
-    ''' Load requirements from a pip requirements file '''
-    with open(filename, 'r') as fd:
+    """Load requirements from a pip requirements file"""
+    with open(filename, "r") as fd:
         lines = []
         for line in fd:
             line.strip()
@@ -35,29 +35,30 @@ def parse_requirements(filename):
                 lines.append(line)
     return lines
 
-requirements = parse_requirements('requirements.txt')
+
+requirements = parse_requirements("requirements.txt")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(
-        name='ipy_course_tools',
-        description='IPython - Jupyter course tools',
-        long_description='\n\n'.join([readme, changes]),
-        license='MIT license',
-        url='https://github.com/matek-alphacruncher/ipy_course_tools',
+        name="ipy_course_tools",
+        description="IPython - Jupyter course tools",
+        long_description="\n\n".join([readme, changes]),
+        license="MIT license",
+        url="https://github.com/edualphacruncher/ipy-course-tools",
         version=version,
-        author='Kovacs Mae',
-        author_email='mate.kovacs@alphacruncher.com',
-        maintainer='Kovacs Mae',
-        maintainer_email='mate.kovacs@alphacruncher.com',
+        author="Kovacs Mae",
+        author_email="mate.kovacs@alphacruncher.com",
+        maintainer="Kovacs Mae",
+        maintainer_email="mate.kovacs@alphacruncher.com",
         install_requires=requirements,
-        keywords=['ipy_course_tools'],
-        package_dir={'': 'src'},
-        packages=find_packages('src'),
+        keywords=["ipy_course_tools"],
         zip_safe=False,
-        classifiers=['Development Status :: 3 - Alpha',
-                     'Intended Audience :: Developers',
-                     'Programming Language :: Python :: 3.6',
-                     'Programming Language :: Python :: 3.7',
-                     'Programming Language :: Python :: 3.8']
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+        ],
     )
