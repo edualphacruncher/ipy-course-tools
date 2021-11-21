bc45867e90a1b0b389af34249a1f86b5c315504c
Mate Kovacs <mate.kovacs@alphacruncher.com>
2021-11-21 16:43:25 +0100

    Version bump

--- ipy_course_tools/__init__.py
+++ ipy_course_tools/__init__.py
@@ -1,4 +1,4 @@
 from ipy_course_tools.formula import *
 
-__version__ = "0.1.8"  #: the working version
-__release__ = "0.1.8"  #: the release version
+__version__ = "0.1.9"  #: the working version
+__release__ = "0.1.9"  #: the release version
